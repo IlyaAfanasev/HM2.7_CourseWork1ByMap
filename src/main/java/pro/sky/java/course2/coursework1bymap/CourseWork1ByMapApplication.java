f7264b5fc04efaf6a74546aef54ac977aee057c1
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-24 19:48:51 +0400

    initial commit

---
+++ src/main/java/pro/sky/java/course2/coursework1bymap/CourseWork1ByMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.coursework1bymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork1ByMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork1ByMapApplication.class, args);
+    }
+
+}
